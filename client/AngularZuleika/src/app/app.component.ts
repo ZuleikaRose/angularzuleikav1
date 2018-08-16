a33226e01333991eea2821f4e7c52db05880fc0d
ZuleikaRose <Rose.Zuleika@gmail.com>
2018-08-16 16:38:52 -0700

    Add Dependencies to App Module, Set up Navbar and Footer, added logo & images, Set up Home Page, REST Service, Data Service, and Message Component

--- client/AngularZuleika/src/app/app.component.ts
+++ client/AngularZuleika/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularZuleika';
+  searchTerm = " ";
+  isCollapsed = true;
+
+  get token() {
+    return localStorage.getItem('token');
+  }
+
+  collapse() {
+    this.isCollapsed = true;
+  }
+
+  closeDropdown(dropdown) {
+    dropdown.close();
+  }
+
+  logout() {}
+
+  search() {}
 }
--- client/AngularZuleika/src/app/app.module.ts
+++ client/AngularZuleika/src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+
+import { RestApiService } from "./rest-api.service";
+import { DataService } from './data.service';
+import { MessageComponent } from './message/message.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MessageComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+    ,AppRoutingModule
+    ,NgbModule.forRoot()
+    ,FormsModule
+    ,HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [RestApiService, DataService]
+  ,bootstrap: [AppComponent]
 })
 export class AppModule { }
