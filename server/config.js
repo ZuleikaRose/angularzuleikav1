f95f8d4009d3a07a9ec11507049fcae633baa428
ZuleikaRose <Rose.Zuleika@gmail.com>
2018-08-16 14:52:01 -0700

    added test file to ensure masterbranch and local files are working

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: "mongodb://alice:abc123@ds223812.mlab.com:23812/angularzuleikav1webapplication"
+    ,port: "3030"
+    ,secret: "AliceRose878745"
+}
---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const config = require("./config");
+
+
+const app = express();
+
+
+// useMongoClient has depreciated. Only include { useMongoClient: true } only if your mongoose version is < 5.0.0
+// DeprecationWarning: current URL string parser is deprecated, and will be removed in a future version. To use the new parser, pass option { useNewUrlParser: true } to MongoClient.connect.
+
+
+mongoose.connect(config.database, { useNewUrlParser: true }, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Connected to the database');
+    }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(cors());
+
+app.get("/", (req, res, next) => {
+    res.json({
+        user: "Alice Rose"
+    });
+});
+
+app.listen(config.port, err => {
+    console.log("Magic happens on port awesome " + config.port);
+});
