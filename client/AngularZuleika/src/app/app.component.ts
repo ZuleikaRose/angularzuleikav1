8fcf82dfab7f6ce4287aaad9a52c6ac3708d579f
ZuleikaRose <Rose.Zuleika@gmail.com>
2018-08-17 19:05:39 -0700

    Get Profile Logic, Set up Profile Page and Settings Page, Created Address Page

--- client/AngularZuleika/src/app/app.component.ts
+++ client/AngularZuleika/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
 import { Router } from '@angular/router';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,9 @@ export class AppComponent {
   searchTerm = '';
   isCollapsed = true;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private data: DataService) {
+    this.data.getProfile();
+  }
 
   get token() {
     return localStorage.getItem('token');
@@ -26,6 +28,7 @@ export class AppComponent {
   }
 
   logout() {
+    this.data.user = {};
     localStorage.clear();
     this.router.navigate(['']);
   }
